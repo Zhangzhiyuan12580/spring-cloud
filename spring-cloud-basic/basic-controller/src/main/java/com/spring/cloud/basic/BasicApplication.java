2188e0c0e41808e18e50eed8f95852275b54e875
zzy825065254 <825065254@qq.com>
2019-03-31 23:03:15 +0800

    服务降级是 在服务调用方降级

--- spring-cloud-basic/basic-controller/src/main/java/com/spring/cloud/basic/BasicApplication.java
+++ spring-cloud-basic/basic-controller/src/main/java/com/spring/cloud/basic/BasicApplication.java
@@ -2,9 +2,7 @@ package com.spring.cloud.basic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
@@ -12,8 +10,6 @@ import org.springframework.data.web.config.EnableSpringDataWebSupport;
 @EnableDiscoveryClient
 @EnableFeignClients
 @EnableSpringDataWebSupport
-@EnableHystrix
-@EnableCircuitBreaker
 public class BasicApplication {
 
     public static void main(String[] args) {

3acd1cb30344b601663060ecdc9b657f4c6502a6
zzy825065254 <825065254@qq.com>
2019-04-21 23:59:25 +0800

    oauth2

--- spring-cloud-bookstore/bookstore-controller/src/main/java/com/spring/cloud/bookstore/BookstoreApplication.java
+++ spring-cloud-bookstore/bookstore-controller/src/main/java/com/spring/cloud/bookstore/BookstoreApplication.java
@@ -5,12 +5,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @Log4j2
 @SpringBootApplication(scanBasePackages = {"com.spring.cloud.bookstore", "com.spring.cloud.basic.feign"})
 @EnableDiscoveryClient
 @EnableHystrix
 //@EnableBinding(Sink.class)
+@EnableResourceServer
 public class BookstoreApplication {
 
     public static void main(String[] args) {
